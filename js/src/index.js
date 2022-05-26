64a8a59f8f7eb5aa0734e0c6485b9d100feee8cd
frodekirkholt <frodekirkholt@penguin>
2022-05-26 08:03:36 +0200

    Remove hack to make routing work in IE

--- js/src/index.js
+++ js/src/index.js
@@ -58,57 +58,6 @@ window.onbeforeunload = function(event) {
     }
 };
 
-// Ugly hack to work around https://github.com/MithrilJS/mithril.js/issues/1734
-function getHashPath() {
-	return location.hash.replace(/^#/, '')
-}
-
-var popstatePath;
-window.addEventListener('popstate', function() {
-	// This event should trigger before hashchange, but IE11 fails to trigger it
-	// on back button click. We save the hash path to confirm that it happened.
-	popstatePath = getHashPath()
-}, false)
-
-var hashchangeTimeoutRef;
-window.addEventListener('hashchange', function() {
-	// This event triggers after popstate, and is more reliable in IE11.
-
-	// We cancel timeout in the rare case that another hash change happened in
-	// the time frame when doing a double check.
-	clearTimeout(hashchangeTimeoutRef)
-
-	let hashPath = getHashPath()
-	if (popstatePath != hashPath) {
-		// The popstate event never happened. This should be IE11. We need to
-		// force it.
-
-		m.route.set(hashPath, undefined, {
-			replace: true, // To let the browser navigate back
-		})
-	} else {
-		// The popstate event triggered, and we should be good, except...
-
-	}
-
-	// Despite all our efforts, Mithril does not recognize the popstate event
-	// occasionally. Need to double check it all went fine. And what's more, if
-	// it goes fine, it will happen in the future – hence the timeout.
-
-	// To reproduce in IE11, click cart then book then back then a different
-	// book then cart.
-	hashchangeTimeoutRef = setTimeout(function() {
-		hashPath = getHashPath()
-		if (m.route.get() != hashPath) {
-			// Mithril failed to recognize the new path, so we need to force
-			// it.
-
-			m.route.set(hashPath, undefined, {
-				replace: true, // To let the browser navigate back
-			})
-		}
-	}, 100)
-}, false)
 
 var $header = $('#header');
 m.mount($header[0], header);
