aca069183e87ce383ffaed4b50f3afb913a88fc7
frodekirkholt <frodekirkholt@penguin>
2022-07-15 11:27:26 +0200

    Lazy load codemirror

--- js/src/codefield.js
+++ js/src/codefield.js
@@ -1,7 +1,3 @@
-var cm = require("codemirror")
-var sql = require("@codemirror/lang-sql")
-var json = require("@codemirror/lang-json")
-
 var editors = []
 
 var Codefield = {
@@ -17,16 +13,22 @@ var Codefield = {
     },
 
     oncreate: function(vnode) {
-        var lang
-        if (vnode.attrs.lang == 'sql') {
-            lang = sql.sql()
-        } else if (vnode.attrs.lang == 'json') {
-            lang = json.json()
-        }
-        editors[vnode.attrs.id] = new cm.EditorView({
-            doc: vnode.attrs.value,
-            extensions: [cm.basicSetup, cm.EditorView.editable.of(vnode.attrs.editable), lang],
-            parent: vnode.dom,
+        Promise.all([
+            import(/* webpackChunkName: "codemirror" */ 'codemirror'),
+            import(/* webpackChunkName: "cm-lang-sql" */ '@codemirror/lang-sql'),
+            import(/* webpackChunkName: "cm-lang-json" */ '@codemirror/lang-json')
+        ]).then(([cm, sql, json]) => {
+            var lang
+            if (vnode.attrs.lang == 'sql') {
+                lang = sql.sql()
+            } else if (vnode.attrs.lang == 'json') {
+                lang = json.json()
+            }
+            editors[vnode.attrs.id] = new cm.EditorView({
+                doc: vnode.attrs.value,
+                extensions: [cm.basicSetup, cm.EditorView.editable.of(vnode.attrs.editable), lang],
+                parent: vnode.dom,
+            })
         })
     },
 
