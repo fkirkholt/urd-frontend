12f72ba95d6146461d242aa6fd7cd816847b4163
Frode Kirkholt <fkirkholt@yahoo.no>
2023-06-05 13:18:05 +0200

    Clean up and comment contents.js

--- js/src/contents.js
+++ js/src/contents.js
@@ -1,11 +1,10 @@
-var Stream = require('mithril/stream')
 var get = require('just-safe-get')
 var config = require('./config')
 var Diagram = require('./diagram')
 
 var Contents = {
 
-  oninit: function(vnode) {
+  oninit: function() {
     $('#right_content').hide()
 
     $('#progress [value="OK"]').on('click', function() {
@@ -16,27 +15,10 @@ var Contents = {
     })
   },
 
-  check_display: function(item) {
-    if (typeof item == 'object') {
-      Object.keys(item.subitems).map(function(label) {
-        var subitem = item.subitems[label]
-        if (typeof subitem == 'object') {
-          subitem.display = Stream('none')
-          if (item.display() == 'none') {
-            item.display = subitem.display
-          }
-        } else {
-          var object = get(ds.base, subitem, ds.base.tables[subitem])
-          if (object === undefined) return
-          if (object.type != 'reference' || config.admin == true) {
-            item.display('block')
-          }
-        }
-        return Contents.check_display(subitem)
-      })
-    }
-  },
-
+  // Get display property for heading
+  // If all underlying tables should be hidden, then the
+  // returned display property is 'none', and the heading
+  // isn't shown
   display_header: function(node, display) {
     display = display || 'none'
     Object.keys(node.subitems).map(function(label) {
@@ -54,16 +36,18 @@ var Contents = {
     return display
   },
 
-
-  draw_node: function(label, node, level) {
+  draw_node: function(label, node) {
+    // If the node is just a heading
     if (typeof node == 'object' && !node.item) {
       var display = node.expanded ? 'block' : 'none'
+      // Decides if the header should be shown or not
       var display_header = Contents.display_header(node)
       return m('.module', {
         class: node.class_module,
         style: 'display:' + display_header
       }, [
           m('span.nowrap', [
+            // Draw expansion icons
             m('i', {
               class: [
                 node.expanded
@@ -73,16 +57,17 @@ var Contents = {
                 'w1 tc',
                 'light-silver'
               ].join(' '),
-              onclick: function(e) {
+              onclick: function() {
                 node.expanded = !node.expanded
               }
             }),
+            // Draw label
             m('.label', {
               class: [
                 node.class_label,
                 'di b pointer'
               ].join(' '),
-              onclick: function(e) {
+              onclick: function() {
                 node.expanded = !node.expanded
               },
               oncontextmenu: function(event) {
@@ -93,10 +78,12 @@ var Contents = {
                 return false
               }
             }, label),
+            // Draw table count if in admin mode
             !node.count || !config.admin ? '' : m('span', {
               class: 'ml2 light-silver'
             }, '(' + node.count + ')'),
           ]),
+          // Draw tables in group if group is expanded
           m('.content', {
             class: node.class_content,
             style: 'display: ' + display
@@ -108,12 +95,13 @@ var Contents = {
                   if (obj === undefined) return
                   label = obj.label
                 }
-                return Contents.draw_node(label, subitem, level + 1)
+                return Contents.draw_node(label, subitem)
               })
             ])
         ])
     } else {
       var subitems
+      // If this is a table with subordinate tables
       if (typeof node == 'object') {
         subitems = node.subitems
         item = node.item
@@ -122,120 +110,95 @@ var Contents = {
         subitems = false
         item = node
       }
-      var object = get(ds.base, item, ds.base.tables[item])
-      var grid_idx_name = object.name + '_grid_idx'
-      var grid_defined = false
-      if (object.indexes && object.indexes[grid_idx_name]) {
-        grid_defined = true
-      }
+      // For now only tables are accepted in contents
+      var table = get(ds.base, item, ds.base.tables[item])
       if (item.indexOf('.') == -1) item = 'tables.' + item
       if (
-        ((object.hidden || object.type == 'list') && !config.admin)
-          || ['xref', 'link', 'ext'].includes(object.type)
+        ((table.hidden || table.type == 'list') && !config.admin)
+          || ['xref', 'link', 'ext'].includes(table.type)
       ) {
         return
       }
-      var icon = object.type && (object.type == 'list') 
+      var icon = table.type && (table.type == 'list') 
         ? 'fa-list'
         : 'fa-table'
-      var icon_color = object.hidden ? 'moon-gray' : 'silver'
-      var title = object.type && (object.type.includes('reference'))
-        ? 'Referansetabell'
-        : null
-      var display = object.type && (object.type.includes('reference')) &&
-        !config.admin && !grid_defined
-        ? 'none'
-        : 'inline'
+      var icon_color = table.hidden ? 'moon-gray' : 'silver'
 
       return m('div', {
-        class: ds.table && ds.table.name == object.name
+        class: ds.table && ds.table.name == table.name
           ? 'bg-light-gray nowrap'
           : 'nowrap',
-        oncontextmenu: function(event) {
-          Contents.context_table = object
-
-          var hidden_txt = object.hidden
-            ? 'Vis tabell'
-            : 'Skjul tabell'
-          $('ul#context-table li.hide').html(hidden_txt)
-
-          var type_txt = object.type == 'list'
-            ? 'Sett til datatabell'
-            : 'Sett til referansetabell'
-          $('ul#context-table li.type').html(type_txt)
-
-          $('ul#context-table')
-            .css({ top: event.clientY, left: event.clientX })
-            .toggle()
-
-          return false
-        }
       }, [
+          // Draw expansion icon for tables having subordinate tables
           typeof node != 'object' ? '' : m('i', {
             class: [
               'w1 tc light-silver fa',
               node.expanded ? 'fa-angle-down' : 'fa-angle-right',
             ].join(' '),
-            style: (display == 'none' || display_chevron == 'none')
-              ? 'display: none'
-              : '',
-            onclick: function(e) {
+            style: display_chevron == 'none' ? 'display: none' : '',
+            onclick: function() {
               node.expanded = !node.expanded
             }
           }),
+          // Draw icon indicating table type
           m('i', {
             class: [
               icon_color + ' mr1 fa ' + icon,
+              // Indent icon correctly when there is no expansjon icon
               (typeof node == 'object' && display_chevron == 'block')
                 ? ''
                 : 'ml3'
             ].join(' '),
-            style: 'display:' + display,
-            title: title
           }),
+          // Draw table name as link
           m('a', {
             class: [
               'black underline-hover nowrap',
-              object.description ? 'dot' : 'link',
-              object.type == 'view' ? 'i' : ''
+              table.description ? 'dot' : 'link',
+              table.type == 'view' ? 'i' : ''
             ].join(' '),
-            title: object.description ? object.description : '',
-            style: 'display:' + display,
+            title: table.description ? table.description : '',
             href: '#/' + ds.base.name + '/' + (config.tab || 'data') +
-              '/' + object.name,
+              '/' + table.name,
             onclick: function() {
               Diagram.type = 'table'
-              Diagram.root = object.name
+              Diagram.root = table.name
+            },
+            oncontextmenu: function(event) {
+              Contents.context_table = table
+
+              var hidden_txt = table.hidden
+                ? 'Vis tabell'
+                : 'Skjul tabell'
+              $('ul#context-table li.hide').html(hidden_txt)
+
+              var type_txt = table.type == 'list'
+                ? 'Sett til datatabell'
+                : 'Sett til referansetabell'
+              $('ul#context-table li.type').html(type_txt)
+
+              $('ul#context-table')
+                .css({ top: event.clientY, left: event.clientX })
+                .toggle()
+
+              return false
             }
           }, label),
-          !object.rowcount || !config.admin ? '' : m('span', {
+          // Draw rowcount if in admin mode
+          !table.rowcount || !config.admin ? '' : m('span', {
             class: 'ml2 light-silver',
-            style: 'display:' + display
-          }, '(' + object.rowcount + ')'),
+          }, '(' + table.rowcount + ')'),
+          // Draw subordinate tables for expanded main table
           !subitems || !node.expanded ? '' : m('.content', {
             style: 'margin-left:' + 18 + 'px',
           }, [
               Object.keys(subitems).map(function(label) {
                 var subitem = subitems[label]
-                return Contents.draw_node(label, subitem, level + 1)
+                return Contents.draw_node(label, subitem)
               })
             ])
         ])
     }
-  },
-
-  draw_fkeys: function(node, def) {
-    var item = node.item ? node.item : node
-    var object = get(ds.base, item, ds.base.tables[item])
-    Diagram.draw_fkeys(object, def, ds.base.contents[module])
-
-    if (!node.subitems) {
-      return
-    }
-
-    Object.values(node.subitems).map(function(subnode) {
-      Contents.draw_fkeys(subnode, def)
-    })
   },
 
   view: function() {
@@ -243,91 +206,101 @@ var Contents = {
 
     if (!ds && !ds.base.contents) return
 
-    return [m('.contents', { class: "flex" }, [
-      m('ul#context-module', {
-        class: 'absolute left-0 bg-white list pa1 shadow-5 dn pointer z-999'
-      }, [
-          m('li', {
-            class: 'hover-blue',
-            onclick: function() {
-              Diagram.type = 'module'
-              Diagram.root = Contents.context_module
-              $('ul#context-module').hide()
-            }
-          }, 'Vis diagram')
-        ]),
-      m('ul#context-table', {
-        class: 'absolute left-0 bg-white list pa1 shadow-5 dn pointer z-999'
-      }, [
-          config.tab == 'data' ? '' : m('li', {
-            class: 'hover-blue',
-            onclick: function() {
-              Diagram.type = 'custom'
-              Diagram.add_path(Contents.context_table)
-              $('ul#context-table').hide()
-            }
-          }, 'Vis koblinger til denne tabellen'),
-          m('li.hide', {
-            class: 'hover-blue',
-            onclick: function() {
-              var tbl = Contents.context_table
-              tbl.hidden = !tbl.hidden
-              $('ul#context-table').hide()
+    return [
+      m('.contents', { class: "flex" }, [
+        // Context menu for modules (table groups)
+        m('ul#context-module', {
+          class: 'absolute left-0 bg-white list pa1 shadow-5 dn pointer z-999'
+        }, [
+            m('li', {
+              class: 'hover-blue',
+              onclick: function() {
+                Diagram.type = 'module'
+                Diagram.root = Contents.context_module
+                $('ul#context-module').hide()
+              }
+            }, 'Vis diagram')
+          ]),
+        // Context menu for single tables
+        m('ul#context-table', {
+          class: 'absolute left-0 bg-white list pa1 shadow-5 dn pointer z-999'
+        }, [
+            // Show links to this table
+            config.tab == 'data' ? '' : m('li', {
+              class: 'hover-blue',
+              onclick: function() {
+                Diagram.type = 'custom'
+                Diagram.add_path(Contents.context_table)
+                $('ul#context-table').hide()
+              }
+            }, 'Vis koblinger til denne tabellen'),
+            // Hide table
+            m('li.hide', {
+              class: 'hover-blue',
+              onclick: function() {
+                var tbl = Contents.context_table
+                tbl.hidden = !tbl.hidden
+                $('ul#context-table').hide()
 
-              ds.set_cfg_value(tbl, 'hidden', tbl.hidden)
-            }
-          }, 'Skjul tabell'),
-          m('li.type', {
-            class: 'hover-blue',
-            onclick: function() {
-              console.log('valgte contextmenyitem')
-              var tbl = Contents.context_table
-              tbl.type = tbl.type == 'table'
-                ? 'list'
-                : 'table'
-              $('ul#context-table').hide()
+                ds.set_cfg_value(tbl, 'hidden', tbl.hidden)
+              }
+            }, 'Skjul tabell'),
+            // Set table to list (i.e. lookup table)
+            m('li.type', {
+              class: 'hover-blue',
+              onclick: function() {
+                var tbl = Contents.context_table
+                tbl.type = tbl.type == 'table'
+                  ? 'list'
+                  : 'table'
+                $('ul#context-table').hide()
 
-              ds.set_cfg_value(tbl, 'type', tbl.type)
-            }
-          }, 'Sett til referansetabell')
+                ds.set_cfg_value(tbl, 'type', tbl.type)
+              }
+            }, 'Sett til referansetabell')
+          ]),
+        // Draw select box for choosing schema within database.
+        // Postgres has this structure.
+        m('.list', { class: "flex flex-column overflow-auto min-w5" }, [
+          !ds.base.schemata || ds.base.schemata.length < 2
+            ? '' : m('select', {
+              class: 'mb2',
+              onchange: function() {
+                var schema = $(this).val()
+                db_name = ds.base.name.split('.')[0]
+                m.route.set('/' + db_name + '.' + schema)
+              }
+            }, [
+                ds.base.schemata.map(function(schema) {
+                  var selected = (schema == ds.base.schema)
+                  return m('option', {
+                    value: schema,
+                    selected: selected
+                  }, schema)
+                })
+              ]),
+          ds.base.contents && Object.keys(ds.base.contents).length
+            // Draw contents from ds.base.contents
+            ? Object.keys(ds.base.contents).sort().map(function(label) {
+              var item = ds.base.contents[label]
+              var retur = Contents.draw_node(label, item, 3)
+              return retur
+            })
+            // Draw contents from ds.base.tables
+            // NOTE: This never happens, because ds.base.contents is always set
+            : Object.keys(ds.base.tables).map(function(name) {
+              var table = ds.base.tables[name]
+              return Contents.draw_node(table.label, 'tables.' + name, 3)
+            }),
         ]),
-      m('.list', { class: "flex flex-column overflow-auto min-w5" }, [
-        !ds.base.schemata || ds.base.schemata.length < 2
-          ? '' : m('select', {
-            class: 'mb2',
-            onchange: function() {
-              var schema = $(this).val()
-              db_name = ds.base.name.split('.')[0]
-              m.route.set('/' + db_name + '.' + schema)
-            }
-          }, [
-              ds.base.schemata.map(function(schema, idx) {
-                var selected = (schema == ds.base.schema)
-                return m('option', {
-                  value: schema,
-                  selected: selected
-                }, schema)
-              })
-            ]),
-        ds.base.contents && Object.keys(ds.base.contents).length
-          ? Object.keys(ds.base.contents).sort().map(function(label) {
-            var item = ds.base.contents[label]
-            item.display = Stream('none')
-            Contents.check_display(item)
-            var retur = Contents.draw_node(label, item, 3)
-            return retur
-          })
-          : Object.keys(ds.base.tables).map(function(name) {
-            var table = ds.base.tables[name]
-            return Contents.draw_node(table.label, 'tables.' + name, 3)
-          }),
       ]),
-    ]), ds.table || !ds.base.description ? '' : m('div', { class: 'pl5' }, [
+      // Show table description to the right of the contents
+      ds.table || !ds.base.description ? '' : m('div', { class: 'pl5' }, [
         m('b', 'Beskrivelse'),
         m('br'),
         m('p', ds.base.description)
-      ])]
-
+      ])
+    ]
   }
 }
 
