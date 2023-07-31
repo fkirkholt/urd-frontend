a32b0008680f52bff61af7716bc7eb3f62915cb7
Frode Kirkholt <fkirkholt@yahoo.no>
2023-07-31 15:34:22 +0200

    Set html_attributes twice to get all in

--- js/src/datapanel.js
+++ js/src/datapanel.js
@@ -11,6 +11,15 @@ var datapanel = {
         }
       }
     } 
+    for (selector in ds.base.html_attrs) {
+      for (attr in ds.base.html_attrs[selector]) {
+        $(selector).attr(attr, ds.base.html_attrs[selector][attr])
+
+        if (attr == 'data-text') {
+          $(selector).text(ds.base.html_attrs[selector][attr])
+        }
+      }
+    } 
   },
 
   view: function(vnode) {
