1f84062850525f378d9a125684d1ab71796682f3
Frode Kirkholt <fkirkholt@yahoo.no>
2023-08-25 10:20:35 +0200

    Fix errors after loading database list

--- js/src/home.js
+++ js/src/home.js
@@ -1,50 +1,44 @@
-var config = require('./config.js');
-var datapanel = require('./datapanel.js');
-var grid = require('./grid.js');
+var config = require('./config.js')
+var datapanel = require('./datapanel.js')
+var grid = require('./grid.js')
 
 var home = {
 
   load_databases: function() {
 
-    grid.url = '';
+    grid.url = ''
 
     m.request({
       method: 'get',
       url: 'dblist'
     }).then(function(result) {
-      ds.table = result.data;
-      // TODO Burde være unødvendig
-      ds.table.filters = [];
-      // TODO Burde være unødvendig
-      ds.table.sort_fields = {};
+      ds.dblist = result.data
     }).catch(function(e) {
       if (e.code === 401) {
-        $('div.curtain').show();
-        $('#login').show();
-        $('#brukernavn').trigger('focus');
+        $('div.curtain').show()
+        $('#login').show()
+        $('#brukernavn').trigger('focus')
       } else {
         alert(e.response ? e.response.detail : 'An error has occurred.')
       }
-    });
+    })
   },
 
   oninit: function() {
-    ds.table = null;
-
-    if (!ds.table) {
-      home.load_databases();
+    if (!ds.dblist) {
+      home.load_databases()
     }
   },
 
   view: function(vnode) {
-    if (!ds.table) return;
+    if (!ds.dblist) return
 
-    if (config.admin) return m(datapanel);
+    if (config.admin) return m(datapanel)
 
     return m('div', [
       m('h2', 'Databaser'),
       m('ul', [
-        ds.table.records.map(function(post, i) {
+        ds.dblist.records.map(function(post, i) {
           return m('li', [
             m('h4.mt1.mb1', [
               m('a', {
@@ -52,11 +46,11 @@ var home = {
               }, post.columns.label),
             ]),
             m('p.mt1.mb1', post.columns.description)
-          ]);
+          ])
         })
       ])
-    ]);
+    ])
   }
 }
 
-module.exports = home;
+module.exports = home
