0a5112317ede05ba25de17e87f23f1ab7d03725e
Frode Kirkholt <fkirkholt@yahoo.no>
2023-10-02 16:01:45 +0200

    Add missing password.js

---
+++ js/src/password.js
@@ -0,0 +1,68 @@
+var pwd_dialog = {
+
+  view: function() {
+    return m('div', [
+      m('label', { class: 'db' }, 'Eksisterende passord'),
+      m('input[type=password]', {
+        id: 'old_pwd'
+      }),
+      m('label', { class: 'db' }, 'Nytt passord'),
+      m('input[type=password]', {
+        id: 'new_pwd'
+      }),
+      m('label', { class: 'db' }, 'Gjenta passord'),
+      m('input[type=password]', {
+        id: 'rep_pwd'
+      }),
+
+      m('input[type=button]', {
+        class: 'fr mt2',
+        value: 'OK',
+        onclick: function() {
+          old_pwd = $('#old_pwd').val()
+          new_pwd = $('#new_pwd').val()
+          rep_pwd = $('#rep_pwd').val()
+          if (new_pwd != rep_pwd) {
+            $('#new_pwd').val('')
+            $('#rep_pwd').val('')
+            alert('Gjentatt passord er forskjellig fra nytt passord')
+            return
+          } 
+          if (old_pwd == '' || new_pwd == '') {
+            alert('Fyll ut alle felter')
+            return
+          }
+          m.request({
+            method: 'put',
+            url: 'change_password',
+            params: {
+              old_pwd: $('#old_pwd').val(),
+              new_pwd: $('#new_pwd').val()
+            }
+          }).then(function(result) {
+            alert(result.data)
+            $('div.curtain').hide()
+            $('#pwd-dialog').hide()
+          }).catch(function(e) {
+            if (e.code === 401) {
+              alert('Kan ikke logge på. Kontakt administrator.')
+            } else {
+              alert(e.response ? e.response.detail : 'An error has occurred.')
+            }
+          })
+        }
+      }),
+      m('input[type=button]', {
+        class: 'fr mt2',
+        value: 'Avbryt',
+        onclick: function() {
+          $('div.curtain').hide()
+          $('#pwd-dialog').hide()
+        }
+      }),
+    ])
+  }
+
+}
+
+module.exports = pwd_dialog
