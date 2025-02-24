0147728ab8908a4da32d777625700585f1764c03
Frode Kirkholt <fkirkholt@yahoo.no>
2025-02-24 11:48:51 +0100

    Clean up

--- js/src/chart.js
+++ js/src/chart.js
@@ -13,19 +13,14 @@ var Chart = {
             labels: data.map(a => a[Object.keys(a)[0]]),
             datasets:
               Object.keys(data[0]).filter(function(key, idx) {
-                console.log('key', key)
-                console.log('val', data[0][key])
                 if (idx == 0) {
-                  console.log('false')
                   return false
                 }
                 labels.push(key)
                 return true
               }).map(function(key, idx) {
-                console.log('key2', key)
                 return {
                   label: Object.keys(data[0])[idx+1].replace('_', ' '),
-                  // label: key,
                   data: data.map(a => a[Object.keys(a)[idx+1]]),
                   borderWidth: 1
                 }
