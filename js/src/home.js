db480b298dc35b0590e1d55d5c51c13c3f695262
Frode Kirkholt <fkirkholt@yahoo.no>
2025-06-28 17:52:25 +0200

    Tweak file display

--- js/src/home.js
+++ js/src/home.js
@@ -193,7 +193,10 @@ var home = {
         })
       ])
     ]),
-    m('div#file', { class: 'flex flex-column' }, [
+    m('div#file', { 
+      class: 'flex flex-column mw7',
+      style: 'flex-grow: 1'
+    }, [
       !ds.file || ds.file.type == 'dir' ? '' : m('div', { class: 'ml3 mb2'}, [
         m('i', { 
           class: [
