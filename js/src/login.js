87960a94ff7b41ff6452be3de630c8e5d58b62f2
Frode Kirkholt <fkirkholt@yahoo.no>
2025-08-09 11:54:26 +0200

    Fix key for connection cookies

--- js/src/login.js
+++ js/src/login.js
@@ -8,11 +8,11 @@ var login = {
 
   view: function() {
     var cnxn_names = Object.keys(Cookies.get())
-      .filter(key => key.startsWith('urdr-cnxn-'))
+      .filter(key => key.startsWith('urdr-cnxn-')).sort()
 
     var cnxn_options = [{label: 'New connection ...', value: 'new'}]
     cnxn_names.forEach(name => cnxn_options.push({
-      label: name.replace('urdr-cnxn-', ''), 
+      label: JSON.parse(Cookies.get(name)).name || name.replace('urdr-cnxn-', ''), 
       value: name.replace('urdr-cnxn-', '')
     }))
 
@@ -21,14 +21,14 @@ var login = {
       !login.error
         ? '' : m('div', { class: 'red' }, login.msg || 'Logg inn'),
       this.create ? '' : m(Select, {
-        value: login.param.name,
+        value: login.param.cnxn,
         onchange: function() {
           if (this.value == 'new') {
             login.create = true
             login.param = {}
           } else if (this.value) {
             login.param = JSON.parse(Cookies.get('urdr-cnxn-' + this.value))
-            login.param.name = this.value
+            login.param.cnxn = this.value
           }
         },
         class: ds.base.name == 'urdr' ? 'dn' : 'db w-100 mb1',
@@ -41,6 +41,7 @@ var login = {
         class: ds.base.name == 'urdr' ? 'dn' : 'db w-100 mb1',
         onchange: function() {
           login.param.name = this.value
+          login.param.cnxn = this.value.toLowerCase().replace(' ', '-')
         }
       }),
       m(Select, {
@@ -50,7 +51,7 @@ var login = {
         label: 'System',
         placeholder: '-- Choose system --',
         value: login.param.system,
-        disabled: login.param.name ? false : true,
+        disabled: login.param.cnxn ? false : true,
         onchange: function() {
           login.param.system = this.value
         },
@@ -92,7 +93,7 @@ var login = {
         placeholder: $('#system').val() == 'sqlite'
           ? 'Path to folder' : 'Host',
         value: login.param.server,
-        disabled: login.param.name ? false : true,
+        disabled: login.param.cnxn ? false : true,
         onkeyup: function(e) {
           login.param.server = this.value
           return true
@@ -104,7 +105,7 @@ var login = {
         name: 'brukernavn',
         placeholder: 'Brukernavn',
         value: login.param.username,
-        disabled: login.param.name || ds.base.name == 'urdr' ? false : true,
+        disabled: login.param.cnxn || ds.base.name == 'urdr' ? false : true,
         class: 'db w-100 mb1'
       }),
       login.param.system == 'sqlite' && ds.base.name != 'urdr' ? '' : m('input[type=password]', {
@@ -112,7 +113,7 @@ var login = {
         name: 'passord',
         placeholder: 'Passord',
         value: login.param.password,
-        disabled: login.param.name || ds.base.name == 'urdr' ? false : true,
+        disabled: login.param.cnxn || ds.base.name == 'urdr' ? false : true,
         class: 'db w-100 mb1',
         onkeypress: function(e) {
           if (e.which == 13) {
@@ -125,29 +126,32 @@ var login = {
         name: 'database',
         placeholder: 'Database',
         value: login.param.database,
-        disabled: login.param.name ? false : true,
+        disabled: login.param.cnxn ? false : true,
         class: ds.base.name == 'urdr' ? 'dn' : 'db w-100 mb1'
       }),
       m('input[type=button]', {
         id: 'btn_login',
-        value: login.param.server || !login.param.name ? 'Logg inn' : 'Delete',
-        disabled: !login.param.name && ds.base.name != 'urdr',
+        value: login.param.server || !login.param.cnxn ? 'Logg inn' : 'Delete',
+        disabled: !login.param.cnxn && ds.base.name != 'urdr',
         class: 'db w-100',
         onclick: function() {
           if (login.param.server || ds.base.name == 'urdr') {
             login.error = false
             login.create = false
             var param = {};
-            param.cnxn = login.param.name
+            param.cnxn = login.param.cnxn.toLowerCase().replace(' ', '-')
+            param.name = login.param.name || login.param.cnxn
+            console.log('param.name', param.name)
+            console.log('param.cnx', param.cnxn)
             param.system = $('#system').val()
             param.server = $('#server').val().trim()
             param.username = $('#brukernavn').val()
             param.password = $('#passord').val()
             param.database = $('#database').val().trim()
 
-            if (login.param.name) {
-              Cookies.set('urdr-cnxn-' + login.param.name, JSON.stringify(param), { expires: 365 })
-              Cookies.set('urdr-cnxn', login.param.name, { expires: 1 })
+            if (login.param.cnxn) {
+              Cookies.set('urdr-cnxn-' + login.param.cnxn, JSON.stringify(param), { expires: 365 })
+              Cookies.set('urdr-cnxn', login.param.cnxn, { expires: 1 })
             } 
 
             m.request({
@@ -172,7 +176,7 @@ var login = {
               }
             })
           } else {
-            Cookies.remove('urdr-cnxn-' + login.param.name)
+            Cookies.remove('urdr-cnxn-' + login.param.cnxn)
             login.param = {}
           }
         }
