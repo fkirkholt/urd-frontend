acf20f55337a2781c49199122fc03c5838a52242
Frode Kirkholt <fkirkholt@yahoo.no>
2025-10-19 13:04:25 +0200

    Fix table loading twice

--- js/src/toolbar.js
+++ js/src/toolbar.js
@@ -155,7 +155,7 @@ var Toolbar = {
     if (index !== null) {
       query_params.index = index
     }
-    if (offset !== undefined) {
+    if (offset !== undefined && offset !== 0) {
       query_params.offset = offset
     }
     if (Object.keys(ds.table.grid.sort_columns).length > 0) {
